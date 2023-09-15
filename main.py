b92071c6c951746d26ecca871af3b2ca7c25942b
memphismalik <thisismemphismalik@gmail.com>
2023-09-15 02:05:44 +0000

    Sign Up Done

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 import time
 
+import requests
 import streamlit as st
-
+from streamlit_javascript import st_javascript as st_js
+from streamlit.components.v1 import html
 from validate_email import validate_email
 
 
@@ -28,6 +30,15 @@ def sign_in(data):
 
         else:
             try:
+                a = requests.post(
+                    'http://127.0.0.1:8000/create_account',
+                    data={
+                        'email': data[0],
+                        'password': data[1],
+                    }
+                )
+
+                print(a.json())
                 del st.session_state['sign_in']
                 del st.session_state['sign_up']
 
@@ -40,15 +51,47 @@ def sign_in(data):
         st.toast('Tu dois remplir les champs')
 
 
-def sign_up():
-    pass
+def sign_up(data):
+    if len(data) == 3:
+        if not validate_email(data[0]):
+            st.toast('Adresse email invalide')
+
+        elif data[1] != data[2]:
+            st.toast('Les deux mots de passe sont différents')
+
+        elif len(data[1]) < 8:
+            st.toast('Mot de passe trop court')
+
+        else:
+            response = requests.post(
+                'http://127.0.0.1:8000/create_account',
+                data={
+                    'email': data[0],
+                    'password': data[1],
+                }
+            )
+
+            del st.session_state['sign_up']
+
+            if response.json()['code'] == 101:
+                st.session_state['sign_in'] = True
+
+            elif response.json()['code'] == 100:
+                html(
+                    f'<script>localStorage.setItem("token", {response.json()["token"]});</script>'
+                )
+                st.session_state['connected'] = True
+
+
+    else:
+        st.toast('Tu dois remplir les champs')
 
 
 def main():
     if 'sign_in' in st.session_state:
         st.title('Dialogflow/_connexion')
-        email = st.text_input('Adresse email')
-        password = st.text_input('Mot de passe')
+        email = st.text_input('Adresse email', key="4")
+        password = st.text_input('Mot de passe', key="5")
 
         data = []
         for i in [email, password]:
@@ -59,10 +102,16 @@ def main():
 
     elif 'sign_up' in st.session_state:
         st.title('Dialogflow/_création_de_compte')
-        st.text_input('Adresse email')
-        st.text_input('Mot de passe')
-        st.text_input('Répéter le mot de passe')
-        st.button('créer un compte')
+        email = st.text_input('Adresse email', key="2")
+        password = st.text_input('Mot de passe', key="1")
+        repeat_password = st.text_input('Répéter le mot de passe', key="3")
+
+        data = []
+        for i in [email, password, repeat_password]:
+            if len(i) > 0:
+                data.append(i)
+
+        st.button('créer un compte', on_click=sign_up, args=(data,))
 
     elif 'connected' in st.session_state:
         chat_input = st.chat_input()
@@ -79,6 +128,7 @@ def main():
             st.button('CLEAR CHAT')
 
     else:
+        print(st.session_state)
         st.title('Dialogflow')
         st.button('Connexion', on_click=set_sign_in)
         st.button('Création de compte', on_click=set_sign_up)
